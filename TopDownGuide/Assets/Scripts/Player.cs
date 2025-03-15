e8d47741ef5d5475e6d00277136da76d4ac5ee90
Joao <progjvdallagnol2001@gmail.com>
2025-03-15 15:03:33 -0300

    Refactoring and creating a mover script to better inherintance logic

--- TopDownGuide/Assets/Scripts/Player.cs
+++ TopDownGuide/Assets/Scripts/Player.cs
@@ -1,49 +1,10 @@
 using UnityEngine;
 
-public class Player : Fighter {
-    private BoxCollider2D boxCollider;
-    private Vector3 moveDelta;
-    private RaycastHit2D hit;
-    private void Start() {
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
-
+public class Player : Mover {
     private void FixedUpdate() {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        //Reset de moveDelta
-        moveDelta = new Vector3(x,y,0);
-
-        // Swap sprite direction, wether you're going right or left
-        if (moveDelta.x > 0) 
-            transform.localScale = Vector3.one;
-        else if(moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-        
-        //Make sure we can move into this direction by casting a box first, if the box return null, we're free to move
-        hit = Physics2D.BoxCast(transform.position, 
-            boxCollider.size, 
-            0, 
-            new Vector2(0, moveDelta.y), 
-            Mathf.Abs(moveDelta.y * Time.deltaTime), 
-            LayerMask.GetMask("Actor", "Blocking")
-        );
-        if (hit.collider == null) {
-            //Move
-            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
-        }
-
-        hit = Physics2D.BoxCast(transform.position, 
-            boxCollider.size, 
-            0, 
-            new Vector2(moveDelta.x, 0), 
-            Mathf.Abs(moveDelta.x * Time.deltaTime), 
-            LayerMask.GetMask("Actor", "Blocking")
-        );
-        if (hit.collider == null) {
-            //Move
-            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
-        }
+        UpdateMotor(new Vector3(x, y, 0));
     }
 }
