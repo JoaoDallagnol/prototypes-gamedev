bba8c97dcabf6460003c8771fac25110239fd83f
Joao <progjvdallagnol2001@gmail.com>
2025-03-23 17:59:12 -0300

    Fixing some bugs and building the 1.0 version

--- TopDownGuide/Assets/Scripts/Mover.cs
+++ TopDownGuide/Assets/Scripts/Mover.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 public abstract class Mover : Fighter {
+    private Vector3 originalSize;
     protected BoxCollider2D boxCollider;
     protected Vector3 moveDelta;
     protected RaycastHit2D hit;
@@ -8,6 +9,7 @@ public abstract class Mover : Fighter {
     public float xSpeed = 1.0f;
 
     protected virtual void Start() {
+        originalSize = transform.localScale;
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
@@ -17,9 +19,9 @@ public abstract class Mover : Fighter {
 
         // Swap sprite direction, wether you're going right or left
         if (moveDelta.x > 0) 
-            transform.localScale = Vector3.one;
+            transform.localScale = originalSize;
         else if(moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(originalSize.x * -1, originalSize.y, originalSize.z);
 
         // Add a push vector, if any
         moveDelta += pushDirection;
