d8b6cc8b23ac92e90ef21de603c32da3a7267ed7
Joao <progjvdallagnol2001@gmail.com>
2025-03-27 12:03:06 -0300

    Importing Player Sprites and creating the animations and adjusting the player facing direction with a function

--- Top-Down Combat System/Assets/Scripts/PlayerController.cs
+++ Top-Down Combat System/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,14 @@ public class PlayerController : MonoBehaviour {
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
 
     private void Awake() {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable() {
@@ -23,15 +27,29 @@ public class PlayerController : MonoBehaviour {
     
     // GERALMENTE FixedUpdate é bom para Fisicas
     private void FixedUpdate() {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput() {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
     private void Move() {
         // Como estamos utilizando o fixedUpdate, então o fixedDeltaTime é mais performatico
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x) {
+            mySpriteRender.flipX = true;
+        } else {
+            mySpriteRender.flipX = false;
+        }
     }
 }
