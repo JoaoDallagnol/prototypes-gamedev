948514cf97a9740c73fb2ed4e39bad80e3303ddd
Joao <progjvdallagnol2001@gmail.com>
2025-03-28 18:44:35 -0300

    Making the sword swing on both directions

--- Top-Down Combat System/Assets/Scripts/Sword.cs
+++ Top-Down Combat System/Assets/Scripts/Sword.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class Sword : MonoBehaviour {
     private PlayerControls playerControls;
     private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     private void Awake() {
+        playerController =  GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         playerControls = new PlayerControls();
         myAnimator = GetComponent<Animator>();
     }
@@ -17,9 +21,27 @@ public class Sword : MonoBehaviour {
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update() {
+        MouseFollowWithOffSet();
+    }
+
     private void Attack() {
         // fire our sword animation
         
         myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffSet() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+        Vector3 playerPosition = playerController.transform.position;
+
+        float angle = Mathf.Atan2(mousePos.y - playerPosition.y, Mathf.Abs(mousePos.x - playerPosition.x)) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerScreenPoint.x) {
+           activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        } else {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
