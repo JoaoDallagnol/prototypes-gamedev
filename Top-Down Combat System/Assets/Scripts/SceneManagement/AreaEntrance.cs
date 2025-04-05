b98e63b26d916efe592fd03f8152f6a854754d8f
Joao <progjvdallagnol2001@gmail.com>
2025-04-05 15:54:28 -0300

    Making a Fade Transition

--- Top-Down Combat System/Assets/Scripts/SceneManagement/AreaEntrance.cs
+++ Top-Down Combat System/Assets/Scripts/SceneManagement/AreaEntrance.cs
@@ -7,6 +7,7 @@ public class AreaEntrance : MonoBehaviour {
         if (transitionName == SceneManagement.Instance.SceneTransitionName) {
             PlayerController.Instance.transform.position = transform.position;
             CameraController.Instance.SetPlayerCameraFollow();
+            UIFade.Instance.FadeToClear();
         }
     }
 }
