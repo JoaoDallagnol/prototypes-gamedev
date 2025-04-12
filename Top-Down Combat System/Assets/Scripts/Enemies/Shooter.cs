d20101f9a0d063e0adb8591dc6dc543d392ec9cf
Joao <progjvdallagnol2001@gmail.com>
2025-04-12 17:05:57 -0300

    Making the Shooter Burst logic

--- Top-Down Combat System/Assets/Scripts/Enemies/Shooter.cs
+++ Top-Down Combat System/Assets/Scripts/Enemies/Shooter.cs
@@ -1,12 +1,39 @@
+using System.Collections;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour, IEnemy {
     [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float bulletMoveSpeed;
+    [SerializeField] private int burstCount;
+    [SerializeField] private float timeBetweenBurts;
+    [SerializeField] private float restTime = 1f;
+
+    private bool isShooting = false;
 
     public void Attack() {
-        Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
-    
-        GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-        newBullet.transform.right = targetDirection;
+        if (!isShooting) {
+            StartCoroutine(ShootRoutine());
+        }
+    }
+
+    private IEnumerator ShootRoutine() {
+        isShooting = true;
+
+        for (int i = 0; i < burstCount; i++)
+        {
+            Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
+        
+            GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            newBullet.transform.right = targetDirection;
+
+            if (newBullet.TryGetComponent(out Projectile projectile)) {
+                projectile.UpdateMoveSpeed(bulletMoveSpeed);
+            }
+
+            yield return new WaitForSeconds(timeBetweenBurts);    
+        }
+
+        yield return new WaitForSeconds(restTime);
+        isShooting = false;
     }
 }
