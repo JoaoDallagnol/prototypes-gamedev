f61225dc8328d2d2ad4a603ff16436b90cd258b4
Joao <progjvdallagnol2001@gmail.com>
2025-04-13 10:13:17 -0300

    Implementing the Cone of Influence to make the shooter burt more customizable

--- Top-Down Combat System/Assets/Scripts/Enemies/Shooter.cs
+++ Top-Down Combat System/Assets/Scripts/Enemies/Shooter.cs
@@ -7,6 +7,9 @@ public class Shooter : MonoBehaviour, IEnemy {
     [SerializeField] private int burstCount;
     [SerializeField] private float timeBetweenBurts;
     [SerializeField] private float restTime = 1f;
+    [SerializeField] private float projectilePerBurst;
+    [SerializeField][Range(0, 359)] private float angleSpread;
+    [SerializeField] private float startingDistance = 0.1f;
 
     private bool isShooting = false;
 
@@ -16,24 +19,61 @@ public class Shooter : MonoBehaviour, IEnemy {
         }
     }
 
-    private IEnumerator ShootRoutine() {
+    private IEnumerator ShootRoutine()
+    {
         isShooting = true;
+
+        float startAngle, currentAngle, angleStep;
+        TargetCloneOfInfluence(out startAngle, out currentAngle, out angleStep);
 
         for (int i = 0; i < burstCount; i++)
         {
-            Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
-        
-            GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            newBullet.transform.right = targetDirection;
 
-            if (newBullet.TryGetComponent(out Projectile projectile)) {
-                projectile.UpdateMoveSpeed(bulletMoveSpeed);
+            for (int j = 0; j < projectilePerBurst; j++)
+            {
+                Vector2 pos = FindBulletSpawnPos(currentAngle);
+                GameObject newBullet = Instantiate(bulletPrefab, pos, Quaternion.identity);
+                newBullet.transform.right = newBullet.transform.position - transform.position;
+
+                if (newBullet.TryGetComponent(out Projectile projectile))
+                {
+                    projectile.UpdateMoveSpeed(bulletMoveSpeed);
+                }
+
+                currentAngle += angleStep;
             }
 
-            yield return new WaitForSeconds(timeBetweenBurts);    
+            currentAngle = startAngle;
+            yield return new WaitForSeconds(timeBetweenBurts);
+            TargetCloneOfInfluence(out startAngle, out currentAngle, out angleStep);
         }
 
         yield return new WaitForSeconds(restTime);
         isShooting = false;
+    }
+
+    private void TargetCloneOfInfluence(out float startAngle, out float currentAngle, out float angleStep) {
+        Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
+        float targetAngle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
+        startAngle = targetAngle;
+        float endAngle = targetAngle;
+        currentAngle = targetAngle;
+        float halfAngleSpread = 0f;
+        angleStep = 0;
+        if (angleSpread != 0) {
+            angleStep = angleSpread / (projectilePerBurst - 1);
+            halfAngleSpread = angleSpread / 2;
+            startAngle = targetAngle - halfAngleSpread;
+            endAngle = targetAngle + halfAngleSpread;
+            currentAngle = startAngle;
+        }
+    }
+
+    private Vector2 FindBulletSpawnPos(float currentAngle) {
+        float x = transform.position.x + startingDistance * Mathf.Cos(currentAngle * Mathf.Deg2Rad);
+        float y = transform.position.y + startingDistance * Mathf.Sin(currentAngle * Mathf.Deg2Rad);
+
+        Vector2 pos = new Vector2(x, y);
+        return pos;
     }
 }
